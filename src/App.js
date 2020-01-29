1b61815e7dca4cf844e9a0b22c58e1c320eacdc9
Catevika <dominique.bello@outlook.fr>
2020-01-29 15:58:57 +0100

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Switch, Route } from 'react-router-dom';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -17,16 +17,33 @@ class App extends Component {
 		};
 	}
 
-	unsubsribeFromAuth = null;
+	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubsribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+				userRef.onSnapshot(snapShot => {
+					this.setState(
+						{
+							currentUser: {
+								id: snapShot.id,
+								...snapShot.data()
+							}
+						},
+						() => {
+							console.log(this.state);
+						}
+					);
+				});
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
 	componentWillUnmount() {
-		this.unsubsribeFromAuth();
+		this.unsubscribeFromAuth();
 	}
 
 	render() {
