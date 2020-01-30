2a0f33030039db389d31ac3f7c04223c37de9121
Catevika <dominique.bello@outlook.fr>
2020-01-30 18:39:39 +0100

    Added checkout page, checkout item components and updated cart to use selectors from reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,8 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { connect } from 'react-redux';
 
+import { createStructuredSelector } from 'reselect';
+
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
@@ -19,8 +21,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
 	);
 };
 
-const mapStateToProps = state => ({
-	itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+	itemCount: selectCartItemsCount
 });
 
 const mapDispatchToProps = dispatch => ({
