6816736080062fa14b4d1236e6933812f3da16f0
Catevika <dominique.bello@outlook.fr>
2020-01-30 16:31:59 +0100

    Updated our app with reselect library, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,19 +6,25 @@ import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
 	return (
 		<div className='cart-icon' onClick={toggleCartHidden}>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{itemCount}</span>
 		</div>
 	);
 };
+
+const mapStateToProps = state => ({
+	itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+// The use of reselect library allows to memoize (cache the state value) and not re-render a component if the state value does not change
+
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+	[selectCart],
+	cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+	[selectCartItems],
+	cartItems =>
+		cartItems.reduce(
+			(accumulatedQuantity, cartItem) =>
+				accumulatedQuantity + cartItem.quantity,
+			0
+		)
+);
